d9758ba4d04532d587ff73af4ba281394de5ed61
avery <averypeckstl@gmail.com>
2020-06-23 22:58:32 -0500

    Added subtract method

--- Hello.java
+++ Hello.java
@@ -3,9 +3,14 @@ public class Hello {
     String name = new Scanner(System.in).nextLine();
     System.out.println("Hello " + name + "!");
     System.out.println("2 + 2 = " + add());
+    System.out.println("14 - 9 = " + subtract());
   }
 
   public static int add() {
     return 2 + 2;
+  }
+
+  public static int subtract() {
+    return 14 - 9;
   }
 }

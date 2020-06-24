db5f50d78673be5dcd3edc9691b56aed023d5c4b
avery <averypeckstl@gmail.com>
2020-06-23 23:49:12 -0500

    no hardcode of add arguments

--- Hello.java
+++ Hello.java
@@ -2,13 +2,13 @@ public class Hello {
   public static void main(String[] args) {
     String name = new Scanner(System.in).nextLine();
     System.out.println("Hello " + name + "!");
-    System.out.println("2 + 2 = " + add());
+    System.out.println("2 + 2 = " + add(2, 2));
     System.out.println("14 - 9 = " + subtract());
     System.out.println("2 * 10 = " + multiply());	  
   }
 
-  public static int add() {
-    return 2 + 2;
+  public static int add(int a, int b) {
+    return a + b;
   }
 
   public static int subtract() {

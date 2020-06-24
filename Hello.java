3997bf9a2e6c2890cdebef3c4044845598175e66
avery <averypeckstl@gmail.com>
2020-06-23 22:57:44 -0500

    Added method

--- Hello.java
+++ Hello.java
@@ -2,5 +2,10 @@ public class Hello {
   public static void main(String[] args) {
     String name = new Scanner(System.in).nextLine();
     System.out.println("Hello " + name + "!");
+    System.out.println("2 + 2 = " + add());
+  }
+
+  public static int add() {
+    return 2 + 2;
   }
 }
